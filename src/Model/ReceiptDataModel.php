3fef7fc1449c056ab2809f81b5fe117723f1ca3a
trndo <vetal1199@gmail.com>
2018-09-12 13:16:30 +0300

    Add ReceiptDataModel

--- src/Model/ReceiptDataModel.php
+++ src/Model/ReceiptDataModel.php
@@ -18,4 +18,54 @@ class ReceiptDataModel
 
     private $tip;
 
+    /**
+     * @return mixed
+     */
+    public function getSubtotal()
+    {
+        return $this->subtotal;
+    }
+
+    /**
+     * @param mixed $subtotal
+     */
+    public function setSubtotal($subtotal): void
+    {
+        $this->subtotal = $subtotal;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTax()
+    {
+        return $this->tax;
+    }
+
+    /**
+     * @param mixed $tax
+     */
+    public function setTax($tax): void
+    {
+        $this->tax = $tax;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTip()
+    {
+        return $this->tip;
+    }
+
+    /**
+     * @param mixed $tip
+     */
+    public function setTip($tip): void
+    {
+        $this->tip = $tip;
+    }
+
+
+
 }
