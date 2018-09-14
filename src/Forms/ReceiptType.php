861231416cc418c1ca5935041103269d3e13733b
trndo <vetal1199@gmail.com>
2018-09-14 13:09:55 +0300

    Add validation

--- src/Forms/ReceiptType.php
+++ src/Forms/ReceiptType.php
@@ -10,10 +10,14 @@ namespace App\Forms;
 
 use App\Entity\Receipt;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Count;
+use Symfony\Component\Validator\Constraints\Range;
 
 
 class ReceiptType extends AbstractType
@@ -21,9 +25,23 @@ class ReceiptType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('subtotal',TextType::class)
-            ->add('tax',TextType::class)
-            ->add('tip',TextType::class)
+        $builder->add('subtotal',NumberType::class)
+            ->add('tax',IntegerType::class,[
+                'constraints'=>[
+                new Range([
+                        'min'=>1,
+                        'max'=>10
+                    ]
+                )]
+        ])
+            ->add('tip',IntegerType::class,[
+                'constraints'=>[
+                    new Range([
+                            'min'=>1,
+                            'max'=>50
+                        ]
+                    )]
+            ])
             ->add('save',SubmitType::class)
             ->getForm();
     }
--- src/Forms/VisitorsType.php
+++ src/Forms/VisitorsType.php
@@ -10,16 +10,26 @@ namespace App\Forms;
 
 use App\Entity\Visitors;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Count;
+use Symfony\Component\Validator\Constraints\Range;
 
 class VisitorsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('numberOf',TextType::class)
+        $builder->add('numberOf',IntegerType::class,[
+            'constraints'=>
+                new Range([
+                    'min'=>2
+                    ]
+                )
+        ])
             ->add('place',TextType::class)
             ->add('save',SubmitType::class)
             ->getForm();
